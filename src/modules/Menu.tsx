b1becf46254e50bd1cb4fa0f9f83a80e716938bc
JESin10 <shinej1029@gmail.com>
2023-09-11 02:12:57 +0900

    [style] : MenuBar Edit

--- src/modules/Menu.tsx
+++ src/modules/Menu.tsx
@@ -43,54 +43,69 @@ export default function Navbar() {
           <p className="sm:hidden">Jin's Portfolio</p>
           <p className="mtoxl:hidden stom:visibility">Jin's</p>
         </PageTitle>
-        {/* <MainButton
-          id="triggerEl"
-          data-collapse-toggle="navbar-sticky"
-          type="button"
-          aria-controls="navbar-sticky"
-          aria-expanded="false"
-        > */}
-        <ul>
+
+        {/* md~ : Menu */}
+        <div className="sm:hidden">
+          <div className="rounded-md w-fit sm:block flex justify-end" id="navbar-sticky">
+            <MenuContainer id="triggerEl">
+              <li>
+                <MenuTag aria-current="page" onClick={() => aboutComponent?.scrollIntoView({ behavior: "smooth" })}>
+                  About
+                </MenuTag>
+              </li>
+              <li>
+                <MenuTag aria-current="page" onClick={() => projectComponent?.scrollIntoView({ behavior: "smooth" })}>
+                  Project
+                </MenuTag>
+              </li>
+              <li>
+                <MenuTag onClick={() => skillComponent?.scrollIntoView({ behavior: "smooth" })}>Skill</MenuTag>
+              </li>
+            </MenuContainer>
+          </div>
+        </div>
+        {/* ~sm : Menu */}
+        <div className="sm:visibility mtoxl:hidden">
           <MainButton onClick={() => MenuOpenHandler()}>
             <BsList className="font-extrabold text-3xl" />
           </MainButton>
-        </ul>
-        <>
-          {isopen ? (
-            <div className="bg-yellow-400 rounded-md w-fit sm:block flex justify-end" id="navbar-sticky">
-              <MenuContainer>
-                <li>
-                  <MenuTag aria-current="page" onClick={() => aboutComponent?.scrollIntoView({ behavior: "smooth" })}>
-                    About
-                  </MenuTag>
-                </li>
-                <li>
-                  <MenuTag aria-current="page" onClick={() => projectComponent?.scrollIntoView({ behavior: "smooth" })}>
-                    Project
-                  </MenuTag>
-                </li>
-                <li>
-                  <MenuTag onClick={() => skillComponent?.scrollIntoView({ behavior: "smooth" })}>Skill</MenuTag>
-                </li>
-              </MenuContainer>
-            </div>
-          ) : null}
-        </>
+          <>
+            {isopen ? (
+              <div className="bg-Main/90 rounded-md w-40 flex flex-col items-center absolute z-99 right-6">
+                <MenuContainer id="triggerEl">
+                  <li className="w-full hover:bg-Gray_Light/20">
+                    <MenuTag aria-current="page" onClick={() => aboutComponent?.scrollIntoView({ behavior: "smooth" })}>
+                      About
+                    </MenuTag>
+                  </li>
+                  <li className="w-full hover:bg-Gray_Light/20">
+                    <MenuTag aria-current="page" onClick={() => projectComponent?.scrollIntoView({ behavior: "smooth" })}>
+                      Project
+                    </MenuTag>
+                  </li>
+                  <li className="w-full hover:bg-Gray_Light/20">
+                    <MenuTag onClick={() => skillComponent?.scrollIntoView({ behavior: "smooth" })}>Skill</MenuTag>
+                  </li>
+                </MenuContainer>
+              </div>
+            ) : null}
+          </>
+        </div>
       </NavContainer>
     </Nav>
   );
 }
 
 const Nav = tw.nav`
-w-screen h-fit bg-Main font-bold text-white text-3xl
-text-center border-b border-gray-200
-z-10 py-4
+font-bold text-white 
+text-center text-3xl
+w-full h-fit z-50 py-4 fixed
+bg-Main shadow-md shadow-black/40
 `;
 
 const NavContainer = tw.div`
 w-2/3 md:w-full h-fit
 flex flex-wrap items-center justify-between mx-auto
-border-red-500 border-2 border-solid
 `;
 
 const PageTitle = tw.div`
@@ -100,24 +115,27 @@ hover:text-Highlight
 `;
 
 const MenuContainer = tw.ul`
-flex flex-col p-4
-sm:flex-row sm:space-x-8 sm:mt-0 sm:text-sm sm:ml-[40%] sm:font-medium sm:border-0
+flex flex-row space-x-4
+sm:w-full sm:space-x-0
+sm:flex-col sm:my-4 sm:mx-auto 
+items-center 
 `;
 
 const MenuTag = tw.a`
-block cursor-pointer  
-md:w-24 sm:w-fit p-1
-py-2 px-auto border-solid border-Gray_Light border-b-2 outline-none
-sm:text-white sm:bg-transparent
-text-lg font-bold bg-white
-text-Main 
-sm:hover:bg-transparent sm:hover:text-Highlight sm:p-0 
+block cursor-pointer text-lg font-bold
+md:w-24 sm:w-full p-4
+py-2 mx-auto 
+text-white hover:text-Highlight bg-transparent
+sm:px-auto sm:py-2
+hover:bg-Gray_Light/40
+border-solid border-Gray_Light border-b-2 outline-none
+sm:hover:bg-transparent
 `;
 
 const MainButton = tw.button`
 inline-flex items-center p-2
 text-sm text-Gray_Light
-rounded-lg sm:hidden 
+rounded-lg  
 hover:bg-gray-100 hover:text-Main 
 focus:outline-none focus:ring-2 focus:ring-gray-200
 sticky
