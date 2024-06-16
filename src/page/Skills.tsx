20786b8e7820f093a5a7b31135184c1a4f5df167
JESin10 <shinej1029@gmail.com>
2024-06-16 17:50:52 +0900

    [chore] : clean code

--- src/page/Skills.tsx
+++ src/page/Skills.tsx
@@ -8,7 +8,6 @@ export default function Skiils() {
   const [typeCheck, setTypeCheck] = useState({
     SkillType: "ALL",
   });
-
   const [buttonActive, setButtonActive] = useState<string>("ALL");
 
   useEffect(() => {
@@ -17,7 +16,7 @@ export default function Skiils() {
 
   const SkillDatafetch = async () => {
     const res = await MySkillData();
-    console.log(res);
+    // console.log(res);
     setSkillData(res);
     return res;
   };
@@ -56,11 +55,14 @@ export default function Skiils() {
       <SkillWrapper className="SkillWrapper">
         {skillData.length > 0 &&
           skillData.map((item: Skills, index: number) => (
-            <SkillBox $active={buttonActive === `${item.skillType}`}>
+            <SkillBox
+              key={index}
+              $active={buttonActive === `${item.skillType}`}
+            >
               <SkillTitle className={item.skillType}>{item.skill}</SkillTitle>
               <SkillContentBox>
-                {item.desc.map((list) => (
-                  <li> ◻︎ {list}</li>
+                {item.desc.map((list, ListIndex: number) => (
+                  <li key={ListIndex}> ◻︎ {list}</li>
                 ))}
               </SkillContentBox>
             </SkillBox>
@@ -71,41 +73,42 @@ export default function Skiils() {
 }
 
 const Skill = tw.div`
-w-full md:w-11/12 mx-auto px-2
-pb-10
+  w-full md:w-11/12 mx-auto px-2
+  pb-10
 `;
 
 const Title = tw.div`
-font-bold text-3xl w-fit mb-4 p-2
-border-solid border-b-4 border-Main
+  font-bold text-3xl w-fit mb-4 p-2
+  border-solid border-b-4 border-Main
 `;
 
 const SubTitleBtn = tw.button`
-w-fit h-auto p-3 rounded-2xl my-2
-bg-Main text-Highlight font-bold
-cursor-pointer
+  w-fit h-auto p-3 rounded-2xl my-2
+  font-bold text-Highlight bg-Main
+  cursor-pointer
+  hover:border-Main hover:bg-Main/80
 `;
 
 const SkillWrapper = tw.div`
-grid
-sm:flex sm:flex-col
-md:grid-cols-2 md:gap-2
-lg:grid-cols-2 lg:gap-2
-grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-2
-mb-4 
+  grid
+  sm:flex sm:flex-col
+  md:grid-cols-2 md:gap-2
+  lg:grid-cols-2 lg:gap-2
+  grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-2
+  mb-4 
 `;
 
 const SkillBox = tw.div`
-w-full p-6 rounded-md cursor-default
-border-2 border-gray-300 border-solid 
-hover:border-Main hover:bg-gray-100 
-  ${(props: { $active: boolean }) =>
-    props.$active ? "bg-Main/10" : "bg-white"}
+  w-full p-6 rounded-md cursor-default
+  border-2 border-gray-300 border-solid 
+  hover:border-Main hover:bg-gray-100 
+    ${(props: { $active: boolean }) =>
+      props.$active ? "bg-Main/10" : "bg-white"}
 `;
 
 const SkillTitle = tw.div`
-font-bold text-xl text-Main
-sm:text-sm 
+  font-bold text-xl text-Main
+  sm:text-sm 
 `;
 
 const SkillContentBox = tw.ul`mt-3 sm:text-sm`;
